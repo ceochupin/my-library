980c61383935b4d3012dbec6d3f2213d5c1aee08
Vladimir Chupin <ceochupin@yandex.ru>
2025-01-06 22:44:35 +0300

    add book cards in page

--- src/scripts/components/books-data.js
+++ src/scripts/components/books-data.js
@@ -3,36 +3,36 @@ export const booksData = [
     name: 'Название1',
     author: 'Ф.И.О. Автор',
     pages: '123',
-    isRead: 'true',
+    isRead: true,
   },
   {
     name: 'Название2',
     author: 'Ф.И.О. Автор',
     pages: '234',
-    isRead: 'false',
+    isRead: false,
   },
   {
     name: 'Название3',
     author: 'Ф.И.О. Автор',
     pages: '456',
-    isRead: 'false',
+    isRead: false,
   },
   {
     name: 'Название4',
     author: 'Ф.И.О. Автор',
     pages: '567',
-    isRead: 'true',
+    isRead: true,
   },
   {
     name: 'Название5',
     author: 'Ф.И.О. Автор',
     pages: '678',
-    isRead: 'true',
+    isRead: true,
   },
   {
     name: 'Название6',
     author: 'Ф.И.О. Автор',
     pages: '789',
-    isRead: 'false',
-  }
+    isRead: false,
+  },
 ]
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,30 @@
 import '../pages/index.css';
 
 import { booksData } from './components/books-data.js';
+
+const bookTemplate = document.querySelector('#book-template');
+const booksPlace = document.querySelector('.place');
+
+const createCard = (bookData) => {
+  const bookElement = bookTemplate.content.querySelector('.book').cloneNode(true);
+
+  const bookName = bookElement.querySelector('.book__name');
+  const bookAuthor = bookElement.querySelector('.book__author');
+  const bookPagesCount = bookElement.querySelector('.book__pages-count');
+  const bookReading = bookElement.querySelector('.switcher__checkbox');
+  const bookDelete = bookElement.querySelector('.book__delete');
+
+  bookName.textContent = bookData.name;
+  bookAuthor.textContent = bookData.author;
+  bookPagesCount.textContent = bookData.pages;
+  bookReading.checked = bookData.isRead;
+
+  return bookElement;
+};
+
+
+booksData.forEach((book) => {
+  const bookElement = createCard(book);
+
+  booksPlace.append(bookElement);
+})
